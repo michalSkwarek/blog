146a701534466255a2c6350a56dc42aff7fa1eee
Michal <skwarek.michal.adrian@gmail.com>
2017-01-02 21:08:33 +0100

    Update config - add annotation config (hibernate + spring)

---
+++ blog/src/main/java/com/skwarek/blog/configuration/ApplicationContextConfiguration.java
@@ -0,0 +1,28 @@
+package com.skwarek.blog.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+/**
+ * Created by Michal on 16.09.2016.
+ */
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = { "com.skwarek.blog" })
+public class ApplicationContextConfiguration extends WebMvcConfigurerAdapter {
+
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+}
---
+++ blog/src/main/resources/sql/create.sql
@@ -0,0 +1,3 @@
+-- ----------------------------------------------
+-- add few categories
+# INSERT INTO category (id, name) VALUES (1, 'Smartphone');
